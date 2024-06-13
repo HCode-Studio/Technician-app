d3b79cc8e11e95bda3f32e574c02bc0f5f9a5332
HamzaKarfa <hamza.karfa@gmail.com>
2024-06-13 04:40:10 +0200

    push app

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -47,5 +50,11 @@ class User extends Authenticatable
     public function sites()
     {
         return $this->hasMany(Site::class);
+    }
+
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@hcodestudio.fr');
     }
 }
